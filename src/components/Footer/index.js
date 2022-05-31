72cfe3d76ef348d489524e6adc03752319b03f9a
Lady Gaga <jessiezheqi.li@gmail.com>
2022-05-31 16:21:45 -0700

    Update footer styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,18 +3,20 @@ import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>
-      <a href="https://github.com/ladystephani" target="_blank">
-        <FaGithub></FaGithub>
-      </a>
-      <a href="https://www.linkedin.com/in/zheqi-jessie-li" target="_blank">
-        <FaLinkedin></FaLinkedin>
-      </a>
-      <a href="https://twitter.com/ladystephani_" target="_blank">
-        <FaTwitter></FaTwitter>
-      </a>
+    <footer>
+      <div className="flex-row">
+        <a href="https://github.com/ladystephani" target="_blank">
+          <FaGithub size={30}></FaGithub>
+        </a>
+        <a href="https://www.linkedin.com/in/zheqi-jessie-li" target="_blank">
+          <FaLinkedin size={30}></FaLinkedin>
+        </a>
+        <a href="https://twitter.com/ladystephani_" target="_blank">
+          <FaTwitter size={30}></FaTwitter>
+        </a>
+      </div>
       <p>&copy; Copyright 2022 Made with ❤️ by ZHEQI JESSIE LI</p>
-    </div>
+    </footer>
   );
 }
 
